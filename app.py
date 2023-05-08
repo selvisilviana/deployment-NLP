15ea0c46b3aa0704ca7373663770466072080a71
selvisilviana <101259902+selvisilviana@users.noreply.github.com>
2023-05-08 13:47:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@
 # =[Modules dan Packages]========================
 
 from flask import Flask,render_template,request,jsonify
+from flask_ngrok import run_with_ngrok
 import pandas as pd
 import numpy as np
 from joblib import load
@@ -88,7 +89,9 @@ if __name__ == '__main__':
 	model = load('model_spam_tfidf_nb.model')
 
 	# Run Flask di localhost 
-	app.run(host="localhost", port=5000, debug=True)
+	#app.run(host="localhost", port=5000, debug=True)
+	run_with_ngrok(app)
+	app.run()
 	
 	
 
